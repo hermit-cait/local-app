78736b6d5a3d9f71b550f9f3264be55187c9ec16
hermit-cait <63665275+hermit-cait@users.noreply.github.com>
2023-07-16 16:01:00 +0200

    Replace let with var

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 document.getElementById("submit").addEventListener("click", newElement); 
-document.getElementById("submit").addEventListener("keydown", newElement); 
 
-// Fetches list data from localStorage if there is any, otherwise an empty array is created for it
+// Fetches list data from localStorage
 
 if (localStorage.getItem('listKey') == null) {
-  let itemList = [];
+  var itemList = [];
 } else {
   itemList = JSON.parse(localStorage.getItem('listKey'));
 }
 
-// Publishes stored data to the UI
+// Publishes saved data to the UI
 
 function restoreData() {
   for (let i = 0; i < itemList.length; i++) {    
-    let li = document.createElement("li");
-    let storedData = document.createTextNode(itemList[i]);
+    var li = document.createElement("li");
+    var storedData = document.createTextNode(itemList[i]);
     li.appendChild(storedData);
     document.getElementById("myUL").appendChild(li);
   }
@@ -26,21 +25,21 @@ function newElement() {
 
   // Creates an empty li element
 
-  let li = document.createElement("li");
+  var li = document.createElement("li");
 
   // Takes the content of the input form and saves it as inputValue
 
-  let inputValue = document.getElementById("item").value;
+  var inputValue = document.getElementById("item").value;
 
   // Adds and saves each new list item to localStorage
 
   itemList.push(inputValue);
   localStorage.setItem('listKey', JSON.stringify(itemList));
-  let listData = JSON.parse(localStorage.getItem('listKey'));
+  var listData = JSON.parse(localStorage.getItem('listKey'));
 
   // Takes inputValue and saves it in the empty li element
 
-  let todo = document.createTextNode(listData.slice(-1));
+  var todo = document.createTextNode(listData.slice(-1));
   li.appendChild(todo);
 
   // Checks first that the field is not empty... then displays this new inputValue in the UI... and clears the input form
@@ -60,12 +59,12 @@ function newElement() {
 
 document.getElementById("clear").addEventListener("click", clearStorage); 
 document.getElementById("clear").addEventListener("keydown", clearStorage);
-let app = document.getElementById("app")
+var app = document.getElementById("app")
 
 function clearStorage() {
   localStorage.clear();
   document.getElementById("myUL").remove();
-  let myUL = document.createElement("ul");
+  var myUL = document.createElement("ul");
   myUL.setAttribute('id', 'myUL');
   app.appendChild(myUL);
   itemList = [];
